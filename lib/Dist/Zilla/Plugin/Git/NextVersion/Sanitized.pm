975ffc2ca61f578cc29c2d16608a717e010be168
Kent Fredric <kentfredric@gmail.com>
2013-12-01 23:37:21 +1300

    First working case

--- lib/Dist/Zilla/Plugin/Git/NextVersion/Sanitized.pm
+++ lib/Dist/Zilla/Plugin/Git/NextVersion/Sanitized.pm
@@ -3,9 +3,27 @@ use warnings;
 
 package Dist::Zilla::Plugin::Git::NextVersion::Sanitized;
 
-# ABSTRACT:
+# ABSTRACT: Sanitize versions handed to you by Git::NextVersion
+
+=head1 SYNOPSIS
+
+This exist, mostly because I accidentally keep doing
+
+    V=2.6.0 dzil release # shit, I mean 2.006000
+
+Or
+
+    V=2.00001 dzil release # Crap, I can't count, how many 0's is that?
+
+So this wraps L<< C<::Role::Version::Sanitize>|Dist::Zilla::Role::Version::Sanitize >> around
+L<< C<[Git::NextVersion]>|Dist::Zilla::Plugin::Git::NextVersion >>
+
+=cut
 
 use Moose;
+extends 'Dist::Zilla::Plugin::Git::NextVersion';
+with 'Dist::Zilla::Role::Version::Sanitize';
+
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
